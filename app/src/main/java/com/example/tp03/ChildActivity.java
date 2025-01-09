f7c31b1db111b2bae03ad1ba3b38796c732235bd
QuentinTlgn <tauleigq@3il.fr>
2025-01-09 15:13:57 +0100

    TP03 done

--- app/src/main/java/com/example/tp03/ChildActivity.java
+++ app/src/main/java/com/example/tp03/ChildActivity.java
@@ -13,13 +13,21 @@ public class ChildActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_child);
 
-        TextView textView = findViewById(R.id.textView); // Replace with your TextView ID in activity_child.xml
+        TextView textView = findViewById(R.id.textView);
 
         Intent intent = getIntent();
         String firstName = intent.getStringExtra("firstName");
         String lastName = intent.getStringExtra("lastName");
 
         textView.setText("Nom : " + lastName + "\nPrenom : " + firstName);
+    }
+
+    @Override
+    public void finish() {
+        Intent data = new Intent();
+        data.putExtra("valRenvoyee", "OK");
+        setResult(RESULT_OK, data);
+        super.finish();
     }
 
 }
--- app/src/main/java/com/example/tp03/MainActivity.java
+++ app/src/main/java/com/example/tp03/MainActivity.java
@@ -1,15 +1,22 @@
 package com.example.tp03;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     TextView textView;
+    TextView textViewChild;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +24,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textView = findViewById(R.id.txtResult);
+        textViewChild = findViewById(R.id.txtChildReturn);
     }
+
+    ActivityResultLauncher<Intent> myActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) { // There are no request codes
+                        Intent myIntent = result.getData();
+                        String myString = myIntent.getStringExtra("valRenvoyee");
+                        textViewChild.setText("Resultat retourné par l'activité fille : " + myString);
+                        Toast.makeText(MainActivity.this, "OK retournée de l'activité fille !", Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
 
     public void onClick_btnSend(View v){
         String lastName = ((EditText)findViewById(R.id.edtLastName)).getText().toString();
@@ -26,6 +48,6 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, ChildActivity.class);
         intent.putExtra("firstName", firstName);
         intent.putExtra("lastName", lastName);
-        startActivity(intent);
+        myActivityResultLauncher.launch(intent);
     }
 }
